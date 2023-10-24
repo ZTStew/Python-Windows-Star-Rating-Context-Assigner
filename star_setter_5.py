aff73515f25692756e587510d693d3a56db67747
ZTStew <zt.stewart.5@gmail.com>
2023-10-24 00:14:34 -0400

    Current program state. Having difficulty getting Windows to run the script through context menus

---
+++ star_setter_5.py
@@ -0,0 +1,19 @@
+"""
+Description:
+  Program is intended to be run in the Windows Right-Click Context Menu.
+  Program will alter the Star Rating of selected files
+  Key name: StarSetter
+"""
+
+import argparse, time
+
+print("hi")
+time.sleep(5)
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("files", nargs="*")
+# args = parser.parse_args()
+
+# for file_path in args.files:
+#   # Your Python script logic here
+#   print(f"Processing: {file_path}")
