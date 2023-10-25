37f706f2d1eebd23e5f692e617da51ff6e7440ff
ZTStew <zt.stewart.5@gmail.com>
2023-10-25 16:27:41 -0400

    Updated to ensure program gets built

--- build_exe.bat
+++ build_exe.bat
@@ -1,8 +1,14 @@
 @echo off
 
+rem forcefully deletes pre-existing files
+cd ".\Build"
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd ".."
+
 copy ".\star_setter_5.py" ".\Build"
 
 cd ".\Build"
 
-pyinstaller star_setter_5.py
+pyinstaller star_setter_5.py -y
 rem timeout /t 10
--- star_setter_5.py
+++ star_setter_5.py
@@ -7,13 +7,15 @@ Description:
 
 import argparse, time
 
-print("hi")
+# print("hi")
+# time.sleep(5)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("files", nargs="*")
+args = parser.parse_args()
+
+for file_path in args.files:
+  # Your Python script logic here
+  print(f"Processing: {file_path}")
+
 time.sleep(5)
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument("files", nargs="*")
-# args = parser.parse_args()
-
-# for file_path in args.files:
-#   # Your Python script logic here
-#   print(f"Processing: {file_path}")
