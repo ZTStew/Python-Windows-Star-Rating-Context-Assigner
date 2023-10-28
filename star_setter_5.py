59b9a55cc615402084d07a3efc82d2adeb7a84e0
ZTStew <zt.stewart.5@gmail.com>
2023-10-27 23:39:26 -0400

    Working model. Only works on .mp3 files at the moment

--- star_setter_5.py
+++ star_setter_5.py
@@ -17,44 +17,32 @@ Rating Values:
 import os, argparse, time
 from mutagen.id3 import POPM, ID3, COMM
 
-file_path = ".\\Examples\\115.mp3"
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("files", nargs="*")
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("files", nargs="*")
+args = parser.parse_args()
 
-# for file_path in args.files:
-  # Your Python script logic here
-if os.path.splitext(file_path)[1] == ".mp3":
-  print(f"Processing: {file_path}")
+# Loops through selected files
+for file_path in args.files:
+  # Rating method for only .mp3 files
+  if os.path.splitext(file_path)[1] == ".mp3":
+    print(f"Processing: {file_path}")
+    audio = ID3(file_path)
+    
+    try:
+      # Changes rating for .mp3 files in POPM format 
+      audio['POPM:Windows Media Player 9 Series'].rating = 255
+      print(audio)
+    except:
+      # When a Windows file has metadata, it is wrapped in the POPM object.
+      # If there is no metadata, then the POPM object doesn't exist and so the .mp3 file needs to be wrapped in a POPM object
 
-  audio = ID3(file_path)
-  print(audio)
-  
-  try:
-    audio['POPM:Windows Media Player 9 Series'].rating = 128
-    print(audio)
-  except:
-    print("non-popm file")
-    print("###################################################")
-    # audio.add(COMM(encoding=3, rating=128))
-    # audio['POPM:Windows Media Player 9 Series'].rating = 128
+      # Case handles when the non-encapsulated .mp3 file is a TPE2 file
+      print("TPE2 File Detected")
+      audio.add(POPM(email=audio.get("TPE2").text[0], rating=255))
 
-    tpe2_frame = audio.get("TPE2")
-    tpe2_value = tpe2_frame.text[0]
-
-    popm_frame = POPM(email=tpe2_value, rating=128)
-    audio.add(popm_frame)
 
     print(audio)
     audio.save()
 
-    print("###################################################")
-
 # time.sleep(5)
-
-
-
-# print(audio['POPM:Windows Media Player 9 Series'].rating)
-# print(audio)
-# print(type(audio))
