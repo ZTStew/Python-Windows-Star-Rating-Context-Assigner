2d0b24ba0c3754a18617a7754154134acc6d7b94
ZTStew <zt.stewart.5@gmail.com>
2023-10-28 17:45:00 -0400

    Added in versions for 0-4 star ratings

--- build_exe.bat
+++ build_exe.bat
@@ -8,6 +8,11 @@ rem navigates back to base directory
 cd ".."
 
 rem copies python program over to different folder to build it
+copy ".\star_setter_0.py" ".\Build"
+copy ".\star_setter_1.py" ".\Build"
+copy ".\star_setter_2.py" ".\Build"
+copy ".\star_setter_3.py" ".\Build"
+copy ".\star_setter_4.py" ".\Build"
 copy ".\star_setter_5.py" ".\Build"
 copy ".\execute.py" ".\Build"
 
@@ -15,5 +20,10 @@ rem moves to location of copied file
 cd ".\Build"
 
 rem builds python program into .exe
+pyinstaller star_setter_0.py -y
+pyinstaller star_setter_1.py -y
+pyinstaller star_setter_2.py -y
+pyinstaller star_setter_3.py -y
+pyinstaller star_setter_4.py -y
 pyinstaller star_setter_5.py -y
 rem timeout /t 10
