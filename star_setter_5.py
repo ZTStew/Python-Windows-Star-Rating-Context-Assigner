d2a80b68ff73658460c2524f2f2a7203bd86a3e9
ZTStew <zt.stewart.5@gmail.com>
2023-10-27 23:28:58 -0400

    Added method for converting TPE2 files into POPM files

--- star_setter_5.py
+++ star_setter_5.py
@@ -15,9 +15,9 @@ Rating Values:
   5 : 255
 """
 import os, argparse, time
-from mutagen.id3 import POPM, ID3
+from mutagen.id3 import POPM, ID3, COMM
 
-file_path = ".\\Examples\\10.mp3"
+file_path = ".\\Examples\\115.mp3"
 
 # parser = argparse.ArgumentParser()
 # parser.add_argument("files", nargs="*")
@@ -31,8 +31,25 @@ if os.path.splitext(file_path)[1] == ".mp3":
   audio = ID3(file_path)
   print(audio)
   
-  # audio['POPM:Windows Media Player 9 Series'].rating = 128
-  # audio.save()
+  try:
+    audio['POPM:Windows Media Player 9 Series'].rating = 128
+    print(audio)
+  except:
+    print("non-popm file")
+    print("###################################################")
+    # audio.add(COMM(encoding=3, rating=128))
+    # audio['POPM:Windows Media Player 9 Series'].rating = 128
+
+    tpe2_frame = audio.get("TPE2")
+    tpe2_value = tpe2_frame.text[0]
+
+    popm_frame = POPM(email=tpe2_value, rating=128)
+    audio.add(popm_frame)
+
+    print(audio)
+    audio.save()
+
+    print("###################################################")
 
 # time.sleep(5)
 
